ce06852c108d56bd567023555a536798ec139394
Ninassc <ninasepulvedacarvalho@gmail.com>
2026-01-13 00:27:10 -0300

    added vite

--- back/src/server.js
+++ back/src/server.js
@@ -12,6 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req,res) => {
+  res.json({
+    message : "everything is okay!"
+  })
+})
+
 app.use("/movies", movieRoutes);
 app.use("/auth", authRoutes);
 app.use("/watchlist", watchlistRoutes)
---
+++ front/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Register from './pages/register/Register'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
